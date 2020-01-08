9d7b31ee8e781488ea0ae768c5cb2ffc5333ebd9
syall <s.yuan.all@gmail.com>
2020-01-07 20:13:49 -0500

    Start port generation

--- fish.js
+++ fish.js
@@ -1,4 +1,5 @@
 const { spawn } = require('child_process');
+const request = require('request');
 
 const express = require('express');
 const morgan = require('morgan');
@@ -11,18 +12,44 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 
-const myapp = spawn('node', [process.argv.pop()], { ...process.env });
+const bread = require(process.argv.pop());
 
-myapp.stdout.on('data', (data) => {
-    console.log('\x1b[36m%s\x1b[0m', data.toString());
-});
+class PortGenerator {
+    constructor() {
+        this.port = 1025;
+        this.ports = new Set();
+        this.ports.add(parseInt(process.env.FISH_PORT, 10));
+    }
 
-myapp.stderr.on('data', (data) => {
-    console.error('\x1b[36m%s\x1b[0m', data.toString());
-});
+    generate(preferred) {
+        if (preferred && !this.ports.has(preferred)) {
+            this.ports.add(preferred);
+            return preferred;
+        }
+        while (this.ports.has(this.port))
+            this.port++;
+        this.ports.add(this.port);
+        return this.port++;
+    }
+}
 
-app.use('*', (req, res) => {
-    res.json('hello');
-});
+const portGen = new PortGenerator();
+const shelf = [];
+const sub = '\x1b[36m%s\x1b[0m';
+for (const loaf of bread.structure) {
+    const options = {
+        env: {
+            ...process.env,
+            PORT: portGen.generate(loaf.preferredPort),
+            NAME: loaf.name
+        }
+    };
+    const toast = spawn('node', [bread.path], options);
+    toast.stdout.on('data', (data) =>
+        console.log(sub, data.toString()));
+    toast.stderr.on('data', (data) =>
+        console.error(sub, data.toString()));
+    shelf.push({ loaf, toast });
+}
 
 module.exports = app;
